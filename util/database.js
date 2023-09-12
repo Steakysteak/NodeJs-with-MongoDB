453f277447f0820243574d4707ea00be40cbe249
Utkarsh Singh <singhutkarsh5032@gmail.com>
2023-09-12 16:21:08 +0530

    Added Mongo

--- util/database.js
+++ util/database.js
@@ -8,18 +8,32 @@
 // );
 
 // module.exports = sequelize;
-
+const dot = require("dotenv");
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+dot.config()
+
 const mongoConnect = (callback) => {
     MongoClient
-    .connect('mongodb+srv://singhutkarsh5032:<password>@cluster0.6id40bv.mongodb.net/?retryWrites=true&w=majority')
-    .then(result => {
-        console.log('connected!')
-        callback(result);
+    .connect(`mongodb+srv://singhutkarsh5032:${process.env.Password}@cluster0.6id40bv.mongodb.net/shop?retryWrites=true&w=majority`)
+    .then(client => {
+        console.log('ConNecTed!')
+        _db = client.db();
+        callback();
     })
     .catch(err => console.log(err));
 }
 
-module.exports = mongoConnect;
+
+const getDb = () => {
+    if(_db){
+        return _db;
+    }
+    throw 'No Database Found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
